93b1fe4dcb81a41a6cd0869f82475d83a5c46add
Daniel Lee <hexa.bricks@gmail.com>
2025-01-09 19:40:50 -0500

    tuning through advantage scope

--- src/main/java/frc/lib/dashboard/TunableNumber.java
+++ src/main/java/frc/lib/dashboard/TunableNumber.java
@@ -1,28 +1,25 @@
 package frc.lib.dashboard;
 
-import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Tunable Number takes in a number type and publishes an entry to a topic called "tuning" to be
  * used with advantagescope
  */
-public class TunableNumber<T extends Number> {
+public class TunableNumber {
 
-  private AtomicReference<T> number;
   private NetworkTableInstance instance = NetworkTableInstance.getDefault();
   private NetworkTable table = instance.getTable("Tuning");
-  private GenericEntry entry;
+  private DoubleEntry entry;
 
-  public TunableNumber(T number) {
-    this.number.set(number);
+  public TunableNumber(String path, double number) {
+    entry = table.getDoubleTopic(path).getEntry(0.0);    
+    entry.set(number);
   }
 
-  public void publishNumber(T number) {}
-
-  public T getNumber() {
-    return null;
+  public double getNumber() {
+    return entry.get();
   }
 }
