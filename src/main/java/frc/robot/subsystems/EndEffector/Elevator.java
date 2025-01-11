beb00e5ede0b7ae4fae2a953787d26d5fd9ae713
MannanB <mannanb728@gmail.com>
2025-01-11 16:14:57 -0500

    elevator part 1

--- src/main/java/frc/robot/subsystems/EndEffector/Elevator.java
+++ src/main/java/frc/robot/subsystems/EndEffector/Elevator.java
@@ -6,7 +6,9 @@ package frc.robot.subsystems.EndEffector;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
 import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.Constants.ElevatorConstants.*;
@@ -15,13 +17,23 @@ import static frc.robot.Constants.HardwareConstants.*;
 import com.ctre.phoenix6.controls.DynamicMotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.units.measure.MutLinearVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
+import frc.lib.dashboard.TunableNumber;
+import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.HardwareConstants;
 
 /*
@@ -43,10 +55,10 @@ public class Elevator extends SubsystemBase {
     PROCESSOR(processorPosition),
     HPStation(HPStationPosition);
 
-    public final Distance rotations;
+    public final Distance inches;
 
-    private ElevatorPositions(Distance rotations) {
-      this.rotations = rotations;
+    private ElevatorPositions(Distance inches) {
+      this.inches = inches;
     }
   }
 
@@ -55,12 +67,23 @@ public class Elevator extends SubsystemBase {
   private final TalonFX m_ElevatorMotor2;
 
   // define control requests
-  // TODO: check whether or not to use dynamic or expo motion magic (leaning towards dynamic)
   private final DynamicMotionMagicTorqueCurrentFOC m_PositionRequest;
   private final TorqueCurrentFOC m_CharacterizationRequest;
 
   // motor characterization stuff
   private final SysIdRoutine m_Routine;
+
+  private final MutVoltage m_appliedVoltage = Volts.mutable(0);
+  private final MutDistance m_distance = Inches.mutable(0);
+  private final MutLinearVelocity m_velocity = InchesPerSecond.mutable(0);
+
+  // tunable numbers
+  private final TunableNumber upVelocity;
+  private final TunableNumber upAcceleration;
+  private final TunableNumber upJerk;
+  private final TunableNumber downVelocity;
+  private final TunableNumber downAcceleration;
+  private final TunableNumber downJerk;
 
   public Elevator() {
     // initialize motors, using the non drivetrain CANivore bus
@@ -78,14 +101,21 @@ public class Elevator extends SubsystemBase {
         .apply(HardwareConstants.Elevator.getElevatorMotorConfig(true));
     // set the position of the elevator
     // TODO: determine where the "zero" point of the elevator is (floor or fully retracted elevator)
-    m_ElevatorMotor1.setPosition(0);
-    m_ElevatorMotor2.setPosition(0);
+    m_ElevatorMotor1.setPosition(inchesToRotations(ElevatorPositions.HOME.inches));
+    m_ElevatorMotor2.setPosition(inchesToRotations(ElevatorPositions.HOME.inches));
 
     // set up sysid
     m_Routine =
         new SysIdRoutine(
-            new Config(Volts.of(10).per(Seconds), Volts.of(12), Seconds.of(4)),
-            new Mechanism(this::characterizeElevator, null, this));
+            new Config(Volts.of(12).per(Seconds), Volts.of(7), Seconds.of(5)),
+            new Mechanism(this::characterizeElevator, this::logMotors, this));
+
+    upVelocity = new TunableNumber("/Elevator/UV", ElevatorConstants.profileUp[0]);
+    upAcceleration = new TunableNumber("/Elevator/UA", ElevatorConstants.profileUp[1]);
+    upJerk = new TunableNumber("/Elevator/UJ", ElevatorConstants.profileUp[2]);
+    downVelocity = new TunableNumber("/Elevator/DV", ElevatorConstants.profileDown[0]);
+    downAcceleration = new TunableNumber("/Elevator/DA", ElevatorConstants.profileDown[1]);
+    downJerk = new TunableNumber("/Elevator/DJ", ElevatorConstants.profileDown[2]);
   }
 
   /**
@@ -98,16 +128,81 @@ public class Elevator extends SubsystemBase {
    *
    * @param current
    */
-  public void characterizeElevator(Voltage current) {}
+  public void characterizeElevator(Voltage current) {
+    m_ElevatorMotor1.setControl(m_CharacterizationRequest.withOutput(current.in(Volts)));
+    m_ElevatorMotor2.setControl(m_CharacterizationRequest.withOutput(current.in(Volts)));
+  }
 
-  // TODO: make sure this function signature makes sense. DISCUSS ON 1/11/25
+  public void logMotors(SysIdRoutineLog log) {
+    log.motor("elevator1")
+        .voltage(
+            m_appliedVoltage.mut_replace(
+                m_ElevatorMotor1.getMotorVoltage().getValue().in(Volts), Volts))
+        .linearPosition(
+            m_distance.mut_replace(
+                rotationsToInches(m_ElevatorMotor1.getPosition().getValue()).in(Inches), Inches))
+        .linearVelocity(
+            m_velocity.mut_replace(
+                rotationsToInches(m_ElevatorMotor1.getVelocity().getValue()).in(InchesPerSecond),
+                InchesPerSecond));
+    log.motor("elevator2")
+        .voltage(
+            m_appliedVoltage.mut_replace(
+                m_ElevatorMotor2.getMotorVoltage().getValue().in(Volts), Volts))
+        .linearPosition(
+            m_distance.mut_replace(
+                rotationsToInches(m_ElevatorMotor2.getPosition().getValue()).in(Inches), Inches))
+        .linearVelocity(
+            m_velocity.mut_replace(
+                rotationsToInches(m_ElevatorMotor2.getVelocity().getValue()).in(InchesPerSecond),
+                InchesPerSecond));
+  }
+
   /**
    * this function should account for whether the elevator is going up or down and modify parameters
    * like velocity and acceleration
    *
    * @param positions the position to set the elevator to
    */
-  public void setPosition(ElevatorPositions positions) {}
+  public void setPosition(ElevatorPositions position, boolean isAlgae) {
+    if (isAtPosition(position, isAlgae)) {
+      return;
+    }
+    Distance currentPosition = rotationsToInches(m_ElevatorMotor1.getPosition().getValue());
+    double algaeOffset =
+        (isAlgae && (position == ElevatorPositions.L2 || position == ElevatorPositions.L3))
+            ? algaeRemovalOffset.in(Inches)
+            : 0;
+    double target = position.inches.in(Inches) + algaeOffset;
+
+    if (currentPosition.in(Inches) < target) {
+      m_PositionRequest.Velocity = upVelocity.getNumber();
+      m_PositionRequest.Acceleration = upAcceleration.getNumber();
+      m_PositionRequest.Jerk = upJerk.getNumber();
+    } else {
+      m_PositionRequest.Velocity = downVelocity.getNumber();
+      m_PositionRequest.Acceleration = downAcceleration.getNumber();
+      m_PositionRequest.Jerk = downJerk.getNumber();
+    }
+
+    m_ElevatorMotor1.setControl(
+        m_PositionRequest.withPosition(inchesToRotations(Inches.of(target))));
+    m_ElevatorMotor2.setControl(
+        m_PositionRequest.withPosition(inchesToRotations(Inches.of(target))));
+  }
+
+  public boolean isAtPosition(ElevatorPositions position, boolean isAlgae) {
+    double currentPosition =
+        rotationsToInches(m_ElevatorMotor1.getPosition().getValue()).in(Inches);
+    double algaeOffset =
+        (isAlgae && (position == ElevatorPositions.L2 || position == ElevatorPositions.L3))
+            ? algaeRemovalOffset.in(Inches)
+            : 0;
+    return MathUtil.applyDeadband(
+            currentPosition - position.inches.in(Inches) - algaeOffset,
+            ElevatorConstants.deadband.in(Inches))
+        == 0.0;
+  }
 
   private Distance rotationsToInches(Angle rotations) {
     return Inches.of(rotations.in(Rotations) * inchesPerRotation.in(Inches));
@@ -115,6 +210,30 @@ public class Elevator extends SubsystemBase {
 
   private Angle inchesToRotations(Distance inches) {
     return Rotations.of(inches.in(Inches) / inchesPerRotation.in(Inches));
+  }
+
+  private LinearVelocity rotationsToInches(AngularVelocity rotations) {
+    return InchesPerSecond.of(rotations.in(RotationsPerSecond) * inchesPerRotation.in(Inches));
+  }
+
+  private AngularVelocity inchesToRotations(LinearVelocity inches) {
+    return RotationsPerSecond.of(inches.in(InchesPerSecond) / inchesPerRotation.in(Inches));
+  }
+
+  public Command quasistaticForward() {
+    return m_Routine.quasistatic(SysIdRoutine.Direction.kForward);
+  }
+
+  public Command quasistaticReverse() {
+    return m_Routine.quasistatic(SysIdRoutine.Direction.kReverse);
+  }
+
+  public Command dynamicForward() {
+    return m_Routine.dynamic(SysIdRoutine.Direction.kForward);
+  }
+
+  public Command dynamicReverse() {
+    return m_Routine.dynamic(SysIdRoutine.Direction.kReverse);
   }
 
   @Override
