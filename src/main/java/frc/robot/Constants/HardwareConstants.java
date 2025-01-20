6098aa3ac57e2fb3b319c08f30ac8bada3d69019
klzsh <klzsh5@gmail.com>
2025-01-20 13:06:35 -0500

    everything uses the units library now

--- src/main/java/frc/robot/Constants/HardwareConstants.java
+++ src/main/java/frc/robot/Constants/HardwareConstants.java
@@ -35,7 +35,7 @@ public final class HardwareConstants {
     /* define actual constants */
 
     /* Motor Inverts */
-    public static final InvertedValue angleMotorInvert = InvertedValue.CounterClockwise_Positive;
+    public static final InvertedValue angleMotorInvert = InvertedValue.Clockwise_Positive;
     public static final InvertedValue driveMotorInvert = InvertedValue.CounterClockwise_Positive;
 
     /* Angle Encoder Invert */
@@ -43,27 +43,22 @@ public final class HardwareConstants {
         SensorDirectionValue.CounterClockwise_Positive;
 
     /* Swerve Current Limiting (Amps) */
-    public static final int angleCurrentLimit = 80;
-    public static final int angleLowerCurrentThreshold = 40;
+    public static final Current angleCurrentLimit = Amps.of(80);
+    public static final Current angleLowerCurrentThreshold = Amps.of(40);
 
-    public static final boolean angleStatorCurrnetLimitEnable = true;
-    public static final int angleStatorCurrentLimit = 120;
-    public static final int angleStatorCurrentLimitForward = 120;
-    public static final int angleStatorCurrentLimitReverse = -120;
+    public static final Current angleStatorCurrentLimit = Amps.of(120);
+    public static final Current angleStatorCurrentLimitForward = Amps.of(120);
+    public static final Current angleStatorCurrentLimitReverse = Amps.of(-120);
     // Seconds
-    public static final double angleCurrentThresholdTime = 0.50;
-    public static final double driveCurrentThresholdTime = 0.50;
+    public static final Time angleCurrentThresholdTime = Seconds.of(0.50);
+    public static final Time driveCurrentThresholdTime = Seconds.of(0.50);
     // amps
-    public static final int driveCurrentLimit = 80;
-    public static final int driveCurrentLowerThreshold = 40;
+    public static final Current driveCurrentLimit = Amps.of(80);
+    public static final Current driveCurrentLowerThreshold = Amps.of(40);
 
-    public static final boolean driveStatorCurrnetLimitEnable = true;
-    public static final int driveStatorCurrentLimit = 80;
-    public static final int driveStatorCurrentLimitForward = 80;
-    public static final int driveStatorCurrentLimitReverse = -80;
-    // seconds
-    public static final boolean angleEnableCurrentLimit = true;
-    public static final boolean driveEnableCurrentLimit = true;
+    public static final Current driveStatorCurrentLimit = Amps.of(80);
+    public static final Current driveStatorCurrentLimitForward = Amps.of(80);
+    public static final Current driveStatorCurrentLimitReverse = Amps.of(-80);
 
     /*
      * These values are used by the drive falcon to ramp in open loop and closed
@@ -71,8 +66,8 @@ public final class HardwareConstants {
      * We found a small open loop ramp (0.25 sec) helps with tread wear, tipping,
      * etc
      */
-    public static final double openLoopRamp = 0.25;
-    public static final double closedLoopRamp = 0.25;
+    public static final Time openLoopRamp = Seconds.of(0.25);
+    public static final Time closedLoopRamp = Seconds.of(0.25);
     //TODO: tune
     public static final double angleTorqueKP = 50.0;
     public static final double angleTorqueKI = 0.0;
@@ -85,7 +80,7 @@ public final class HardwareConstants {
     public static final double driveTorqueKP = 2;
     public static final double driveTorqueKI = 0.0;
     public static final double driveTorqueKD = 0.0;
-    public static final double driveTorqueKS = 10; // 19.449
+    public static final double driveTorqueKS = 10;
 
     /* Neutral Modes */
     public static final NeutralModeValue angleNeutralMode = NeutralModeValue.Coast;
@@ -99,15 +94,15 @@ public final class HardwareConstants {
       m_DriveConfig.MotorOutput.NeutralMode = driveNeutralMode;
 
       /* Current Limiting */
-      m_DriveConfig.CurrentLimits.SupplyCurrentLimitEnable = driveEnableCurrentLimit;
-      m_DriveConfig.CurrentLimits.SupplyCurrentLimit = driveCurrentLimit;
-      m_DriveConfig.CurrentLimits.SupplyCurrentLowerTime = driveCurrentThresholdTime;
-      m_DriveConfig.CurrentLimits.SupplyCurrentLowerLimit = driveCurrentLowerThreshold;
+      m_DriveConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+      m_DriveConfig.CurrentLimits.SupplyCurrentLimit = driveCurrentLimit.in(Amps);
+      m_DriveConfig.CurrentLimits.SupplyCurrentLowerTime = driveCurrentThresholdTime.in(Seconds);
+      m_DriveConfig.CurrentLimits.SupplyCurrentLowerLimit = driveCurrentLowerThreshold.in(Amps);
 
-      m_DriveConfig.CurrentLimits.StatorCurrentLimitEnable = driveStatorCurrnetLimitEnable;
-      m_DriveConfig.CurrentLimits.StatorCurrentLimit = driveStatorCurrentLimit;
-      m_DriveConfig.TorqueCurrent.PeakForwardTorqueCurrent = driveStatorCurrentLimitForward;
-      m_DriveConfig.TorqueCurrent.PeakReverseTorqueCurrent = driveStatorCurrentLimitReverse;
+      m_DriveConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+      m_DriveConfig.CurrentLimits.StatorCurrentLimit = driveStatorCurrentLimit.in(Amps);
+      m_DriveConfig.TorqueCurrent.PeakForwardTorqueCurrent = driveStatorCurrentLimitForward.in(Amps);
+      m_DriveConfig.TorqueCurrent.PeakReverseTorqueCurrent = driveStatorCurrentLimitReverse.in(Amps);
 
       /* PID Config */
       m_DriveConfig.Slot0.kP = driveTorqueKP;
@@ -116,13 +111,13 @@ public final class HardwareConstants {
       m_DriveConfig.Slot0.kS = driveTorqueKS;
 
       /* Open and Closed Loop Ramping */
-      m_DriveConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = openLoopRamp;
-      m_DriveConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = openLoopRamp;
+      m_DriveConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = openLoopRamp.in(Seconds);
+      m_DriveConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = openLoopRamp.in(Seconds);
 
-      m_DriveConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = closedLoopRamp;
-      m_DriveConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = closedLoopRamp;
+      m_DriveConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = closedLoopRamp.in(Seconds);
+      m_DriveConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = closedLoopRamp.in(Seconds);
       // TODO: see if nessesary
-      m_DriveConfig.ClosedLoopRamps.TorqueClosedLoopRampPeriod = closedLoopRamp;
+      m_DriveConfig.ClosedLoopRamps.TorqueClosedLoopRampPeriod = closedLoopRamp.in(Seconds);
       m_DriveConfig.MotorOutput.ControlTimesyncFreqHz = timeSyncFreq.in(Hertz);
 
       return m_DriveConfig;
@@ -133,15 +128,15 @@ public final class HardwareConstants {
       m_AngleConfig.MotorOutput.NeutralMode = angleNeutralMode;
 
       /* Current Limiting */
-      m_AngleConfig.CurrentLimits.SupplyCurrentLimitEnable = angleEnableCurrentLimit;
-      m_AngleConfig.CurrentLimits.SupplyCurrentLimit = angleCurrentLimit;
-      m_AngleConfig.CurrentLimits.SupplyCurrentLowerTime = angleCurrentThresholdTime;
-      m_AngleConfig.CurrentLimits.SupplyCurrentLowerLimit = angleLowerCurrentThreshold;
+      m_AngleConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+      m_AngleConfig.CurrentLimits.SupplyCurrentLimit = angleCurrentLimit.in(Amps);
+      m_AngleConfig.CurrentLimits.SupplyCurrentLowerTime = angleCurrentThresholdTime.in(Seconds);
+      m_AngleConfig.CurrentLimits.SupplyCurrentLowerLimit = angleLowerCurrentThreshold.in(Amps);
 
-      m_AngleConfig.CurrentLimits.StatorCurrentLimitEnable = angleStatorCurrnetLimitEnable;
-      m_AngleConfig.CurrentLimits.StatorCurrentLimit = angleStatorCurrentLimit;
-      m_AngleConfig.TorqueCurrent.PeakForwardTorqueCurrent = angleStatorCurrentLimitForward;
-      m_AngleConfig.TorqueCurrent.PeakReverseTorqueCurrent = angleStatorCurrentLimitReverse;
+      m_AngleConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+      m_AngleConfig.CurrentLimits.StatorCurrentLimit = angleStatorCurrentLimit.in(Amps);
+      m_AngleConfig.TorqueCurrent.PeakForwardTorqueCurrent = angleStatorCurrentLimitForward.in(Amps);
+      m_AngleConfig.TorqueCurrent.PeakReverseTorqueCurrent = angleStatorCurrentLimitReverse.in(Amps);
 
       /* PID Config */
       m_AngleConfig.Slot0.kP = angleTorqueKP;
