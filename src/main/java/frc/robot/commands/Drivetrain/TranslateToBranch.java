a34e198b79989f207400df3b0e8ef5a8e54d03f7
MannanB <37980685+MannanB@users.noreply.github.com>
2025-02-20 13:24:43 -0500

    Update TranslateToBranch.java

--- src/main/java/frc/robot/commands/Drivetrain/TranslateToBranch.java
+++ src/main/java/frc/robot/commands/Drivetrain/TranslateToBranch.java
@@ -22,66 +22,15 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 @Logged(name = "TranslateToBranch", strategy = Strategy.OPT_IN)
 public class TranslateToBranch extends Command {
 
-  private PIDController xController;
-  private PIDController yController;
-
-  private final AprilTagCamera m_AprilTagCamera;
   private final Swerve m_Swerve;
   private boolean alignLeft;
+  private VisionTranslationController2 visionController;
 
-  private final double targetPitchLeft = -5;
-  private final double targetPitchRight = 0.5;
-  private final double targetYawLeft = 13;
-  private final double targetYawRight = 0;
-
-  private OptionalDouble currentPitch;
-  private OptionalDouble currentYaw;
-
-  @Logged(name = "TranslateToBranch/XCommand", importance = Importance.CRITICAL)
-  private double xCommand;
-  @Logged(name = "TranslateToBranch/YCommand", importance = Importance.CRITICAL)
-  private double yCommand;
-
-  private TunableNumber[] xPID = {
-    new TunableNumber("TranslateToBranch/xP", 0.005),
-    new TunableNumber("TranslateToBranch/xI", 0),
-    new TunableNumber("TranslateToBranch/xD", 0.0001)
-  };
-
-  private TunableNumber[] yPID = {
-    new TunableNumber("TranslateToBranch/yP", 0.001),
-    new TunableNumber("TranslateToBranch/yI", 0),
-    new TunableNumber("TranslateToBranch/yD", 0.0001)
-  };
-
-  private final Map<Double, Set<Integer>> headingToTagIDs;
-  {
-    headingToTagIDs = new HashMap<>();
-    headingToTagIDs.put(Math.toRadians(0), Set.of(18, 7));
-    headingToTagIDs.put(Math.toRadians(60), Set.of(8, 17));
-    headingToTagIDs.put(Math.toRadians(120), Set.of(9, 22));
-    headingToTagIDs.put(Math.toRadians(180), Set.of(10, 21));
-    headingToTagIDs.put(Math.toRadians(240), Set.of(11, 20));
-    headingToTagIDs.put(Math.toRadians(300), Set.of(22, 6));
-  }
-
-  private Set<Integer> correspondingTagIDs;
-  private int tagIDToFocus;
-
-
-
-
-
-
-
-
-  public TranslateToBranch(Swerve swerve, AprilTagCamera camera, boolean alignLeft) {
+  public TranslateToBranch(Swerve swerve, boolean alignLeft, VisionTranslationController2 controller) {
     m_Swerve = swerve;
-    m_AprilTagCamera = camera;
     this.alignLeft = alignLeft;
 
-    xController = new PIDController(xPID[0].getNumber(), xPID[1].getNumber(), xPID[2].getNumber());
-    yController = new PIDController(yPID[0].getNumber(), yPID[1].getNumber(), yPID[2].getNumber());
+    visionController = controller;
 
     addRequirements(m_Swerve);
   }
@@ -90,74 +39,32 @@ public class TranslateToBranch extends Command {
 
   @Override
   public void initialize() {
-    if (alignLeft) {
-      xController.setSetpoint(targetPitchLeft);
-      yController.setSetpoint(targetYawLeft);
-    } else {
-      xController.setSetpoint(targetPitchRight);
-      yController.setSetpoint(targetPitchLeft);
-    }
-    xController.setTolerance(5);
-    yController.setTolerance(5);
-
-    double heading = m_Swerve.getPose().getRotation().getRadians();
-    double nearestHeading = Collections.min(headingToTagIDs.keySet(), 
-    Comparator.comparingDouble(h -> Math.abs(h - heading)));
-    correspondingTagIDs = headingToTagIDs.get(nearestHeading); // get the set of 2 tagIDs that correspond to the nearest heading
-
-    // Determine the tagIDToFocus based on the alliance
-    Alliance alliance = DriverStation.getAlliance().get();
-    if (alliance == Alliance.Red) {
-      tagIDToFocus = correspondingTagIDs.stream()
-        .filter(id -> id >= 6 && id <= 11)
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException("No valid tag ID found for Red alliance"));
-    } else if (alliance == Alliance.Blue) {
-      tagIDToFocus = correspondingTagIDs.stream()
-        .filter(id -> id >= 17 && id <= 22)
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException("No valid tag ID found for Blue alliance"));
-    } else {
-      throw new IllegalStateException("Unknown alliance: " + alliance);
+    visionController.reset(alignLeft);
     }
 
     // tagIDToFocus is the tagID which relates to the nearest heading and the alliance
-  }
 
 
 
   @Override
   public void execute() {
-    xController.setPID(xPID[0].getNumber(), xPID[1].getNumber(), xPID[2].getNumber());
-    yController.setPID(yPID[0].getNumber(), yPID[1].getNumber(), yPID[2].getNumber());
+    ChassisSpeeds speeds = m_controller.update(m_swerve.getPose(), targetPose);
+    m_swerve.setChassisSpeeds(speeds);
     
-    
-    if (m_AprilTagCamera.getTarget(tagIDToFocus).isPresent()) { // ensure tag to focus in view
-      currentPitch = m_AprilTagCamera.getY(tagIDToFocus); // Y is Pitch
-      currentYaw = m_AprilTagCamera.getX(tagIDToFocus); // X is Yaw
-      if (alignLeft) {
-        xCommand = xController.calculate(currentPitch.getAsDouble(), targetPitchLeft);
-        yCommand = yController.calculate(currentYaw.getAsDouble(), targetYawLeft);
-      } else {
-        xCommand = xController.calculate(currentPitch.getAsDouble(), targetPitchRight);
-        yCommand = yController.calculate(currentYaw.getAsDouble(), targetYawRight);
-      }
-
-      // m_Swerve.drive(new Translation2d(xCommand, yCommand), 0); // needs to be robot relative
-      m_Swerve.setRobotRelative(new ChassisSpeeds(xCommand, yCommand, 0));
-    }
   }
 
 
 
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    
+  }
 
 
 
   @Override
   public boolean isFinished() {
     // return false;
-    return (xController.atSetpoint() && yController.atSetpoint());
+    return m_controller.atGoal();
   }
 }
