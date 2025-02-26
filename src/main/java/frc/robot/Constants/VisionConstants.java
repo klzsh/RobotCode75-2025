ef61130da851ecbb24f532a1d4e9ae3d744f8b42
Krish Patel <107934597+krish1patel@users.noreply.github.com>
2025-02-26 14:23:05 -0500

    poss fixed HP Camera Pose

--- src/main/java/frc/robot/Constants/VisionConstants.java
+++ src/main/java/frc/robot/Constants/VisionConstants.java
@@ -119,14 +119,18 @@ public class VisionConstants {
   public static final Transform3d HPCameraPose =
       new Transform3d(
           new Translation3d(
-              Units.inchesToMeters(0), Units.inchesToMeters(-8.033), Units.inchesToMeters(40.25)),
+              Units.inchesToMeters(0),
+              Units.inchesToMeters(-6.033),
+              Units.inchesToMeters(40.25)),
           new Rotation3d(
-              Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(25)));
+              Units.degreesToRadians(0), 
+              Units.degreesToRadians(0), 
+              Units.degreesToRadians(125)));
   public static final Transform3d CageDetectCameraPose =
       new Transform3d(
           new Translation3d(
               Units.inchesToMeters(.863),
-              Units.inchesToMeters(-8.367),
+              Units.inchesToMeters(-7.387),
               Units.inchesToMeters(34.334)),
           new Rotation3d(
               Units.degreesToRadians(0), Units.degreesToRadians(160), Units.degreesToRadians(160)));
