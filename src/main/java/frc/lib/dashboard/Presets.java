4360190005e59433162ce78e96fd045c36b85029
Krish Patel <patelkri0507@hotmail.com>
2025-03-24 21:19:11 -0400

    added 3 piece preset

--- src/main/java/frc/lib/dashboard/Presets.java
+++ src/main/java/frc/lib/dashboard/Presets.java
@@ -4,4 +4,5 @@ public class Presets {
   public static final String LeftSideTwoPiece = "st cr 3 ht 5 bl 6 3";
   public static final String RightSideTwoPiece = "sb el 3 hb 5 fl 3";
   public static final String MiddleOnePiece = "sm dl 3";
+  public static final String LeftSideThreePiece = "st cl 8 ht 5 bl 8 ht 5 br 8";
 }
