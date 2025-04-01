72851f3e79f8a82a160f959bd46e6347235b1b13
MannanB <mannanb728@gmail.com>
2025-04-01 19:33:16 -0400

    Reset Gyro To Similar Face

---
+++ src/main/java/frc/robot/commands/Drivetrain/ResetHeadingToSimilarFace.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Drivetrain;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain.Swerve;
+
+/** resets the heading of the robot (should not be used often) */
+public class ResetHeadingToSimilarFace extends Command {
+  /** Creates a new ResetHeading. */
+  private final Swerve m_Swerve;
+  private double targetHeading;
+
+  public ResetHeadingToSimilarFace(Swerve swerve) {
+    m_Swerve = swerve;
+    addRequirements(m_Swerve);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    targetHeading = Math.round(m_Swerve.getRotation2D().getDegrees() / 60.0) * 60.0;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Swerve.zeroGyro(Rotation2d.fromDegrees(targetHeading));
+    m_Swerve.setPose(
+        new Pose2d(
+            m_Swerve.getPose().getX(), m_Swerve.getPose().getY(), Rotation2d.fromDegrees(targetHeading)));
+    // resets the gyro and pose based on the gyro
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    boolean resetted = false;
+    // see if the pose has actually reset
+    if (m_Swerve.getPose().getRotation().getDegrees()
+        == targetHeading) {
+      resetted = true;
+    } else {
+      resetted = false;
+    }
+    return resetted;
+  }
+}
